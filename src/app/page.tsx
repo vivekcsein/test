f7fb8e6f241008b878096f6b21d7e6e3efda6a1d
Vivek <vivekcse.in@gmail.com>
2023-11-20 14:25:28 +0530

    next commit for test sass 02

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,8 @@
-import "../styles/section/pagecut.scss";
+import Hero from "@/components/Hero";
 export default function Home() {
   return (
     <main>
-      <div className="title">Hello India I love you</div>
-      <h1 className="text-center text-red-600">Hello India congrats</h1>
-      <p className="text-blue-500 font-bold mt-24 text-center">
-        Hello I am vivek lets go for love
-      </p>
+      <Hero />
     </main>
   );
 }
---
+++ src/components/Hero.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+// import "../styles/section/Hero.scss";
+const Hero = () => {
+  return (
+    <div>
+      <div className="title">Hello India I love you</div>
+      <h1 className="text-center text-red-600">Hello India congrats</h1>
+      <p className="text-blue-500 font-bold mt-24 text-center">
+        Hello I am vivek lets go for love
+      </p>
+      <div className="fullScreen">Vivek G aapko badhai</div>
+    </div>
+  );
+};
+
+export default Hero;
