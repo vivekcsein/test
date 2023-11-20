29fd1d46f1666637b314e3ff66ad0cfeb28afa90
Vivek <vivekcse.in@gmail.com>
2023-11-20 14:02:17 +0530

    text sass compiler check

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import "../styles/section/pagecut.scss";
+export default function Home() {
+  return (
+    <main>
+      <div className="title">Hello India I love you</div>
+      <h1 className="text-center text-red-600">Hello India congrats</h1>
+      <p className="text-blue-500 font-bold mt-24 text-center">
+        Hello I am vivek lets go for love
+      </p>
+    </main>
+  );
+}
